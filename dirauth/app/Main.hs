934a3f07dd95d2556edfd5acdee7389898997a87
David Stainton <dstainton415@gmail.com>
2023-06-18 00:22:43 -0400

    Add haskell program to draw dirauth diagram

---
+++ dirauth/app/Main.hs
@@ -0,0 +1,19 @@
+
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
+
+node :: Int -> Diagram B
+node n = text ("dirauth" ++ show n) # fontSizeL 0.08 # fc white
+      <> circle 0.2 # fc green # named n
+
+arrowOpts = with & gaps       .~ small
+                  & headLength .~ local 0.15
+
+tournament :: Int -> Diagram B
+tournament n = atPoints (trailVertices $ regPoly n 1) (map node [1..n])
+  # applyAll [connectOutside' arrowOpts j k | j <- [1 .. n-1], k <- [j+1 .. n]]
+
+example :: Diagram B
+example = tournament 5
+
+main = mainWith example
