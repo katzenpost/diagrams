94b5c14f139acd4ab9d6d9158ba497a99184b022
David Stainton <dstainton415@gmail.com>
2023-06-18 12:53:17 -0400

    update mix1 diagram

--- mix1/app/Main.hs
+++ mix1/app/Main.hs
@@ -8,17 +8,17 @@ import Diagrams.Backend.SVG.CmdLine
 -- Create a small circle labeled with a given string
 circleDiagram :: String -> Diagram B
 circleDiagram s = (text s # fontSizeL 0.1 # fc white)
-                 <> circle 0.2 # fc blue # named s
+                 <> circle 0.2 # fc lightcoral # named s
 
--- Create a horizontal row of circles with given labels
-row :: [String] -> Diagram B
-row labels = hsep 1 $ map circleDiagram labels
+-- Create a vertical column of circles with given labels
+column :: [String] -> Diagram B
+column labels = vsep 0.3 $ map circleDiagram labels
 
 -- Create a diagram with specified clients and 1 mix
 diagram :: Diagram B
-diagram = position [(zero, clientRow), (p2 (width clientRow / 2, -1), mix)]
+diagram = position [(zero, clientColumn), (p2 (width clientColumn + 1, -(height clientColumn / 2)), mix)]
     where
-        clientRow = row ["Tom", "Dick", "Harry", "George", "Nancy"]
+        clientColumn = column ["Tom", "Dick", "Harry", "George", "Nancy"]
         mix = circleDiagram "mix"
 
 -- Create a full mesh of arrows from all clients to the mix
